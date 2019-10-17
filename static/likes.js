3b10b3e803d9a83ab660bca6f511bb5640b73597
Base Camp <basecamp@ssa-406.local>
2019-10-17 15:50:30 -0500

    Changed url in Javascript

--- static/likes.js
+++ static/likes.js
@@ -1,6 +1,6 @@
 const likes = document.querySelector("#likes")
 likes.addEventListener("click", () => {
-    const url = `/entries/${id}/like`
+    const url = `/entries/<int:id>/like`
 
     fetch(url)
         .then(response => response.json())
