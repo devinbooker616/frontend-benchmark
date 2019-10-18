7dd2eb358b94c19276cdb260acb6c974bf2253f8
Base Camp <basecamp@ssa-406.local>
2019-10-18 15:11:27 -0500

    unadded cosnole logs
    changed typo-ish's

--- static/likes.js
+++ static/likes.js
@@ -1,15 +1,16 @@
 const main = document.querySelector("#main")
-const sub = main.querySelectorAll("#sub")
-
+const sub = main.querySelectorAll("#Sub")
 
 
 for (const subs of sub) {
+    console.log("yay")
     const divs = subs.querySelector("#id")
     const like = subs.querySelector("#likes")
     const numOfLikes = like.querySelector("#numOfLikes")
 
     like.addEventListener("click", () => {
-        const url = `/entries/${divs.textContent.trim()}/likes`;
+        console.log("yay")
+        const url = `/entries/${divs.textContent.trim()}/like`;
         console.log("yay")
         fetch(url, { method: 'post' })
             .then(response => response.json())
