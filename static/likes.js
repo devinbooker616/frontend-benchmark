f9d5fd81381c63c698bdeffeba42cb78fdbf78aa
Base Camp <basecamp@ssa-406.local>
2019-10-18 14:58:07 -0500

    revised javascript

--- static/likes.js
+++ static/likes.js
@@ -1,12 +1,21 @@
-const likes = document.querySelector("#likes")
-likes.addEventListener("click", () => {
-    const url = `/entries/<int:id>/like`
+const main = document.querySelector("#main")
+const sub = main.querySelectorAll("#sub")
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            for (const nums of data.likes) {
 
-            }
-        })
-})
+
+for (const subs of sub) {
+    const divs = subs.querySelector("#id")
+    const like = subs.querySelector("#likes")
+    const numOfLikes = like.querySelector("#numOfLikes")
+
+    like.addEventListener("click", () => {
+        const url = `/entries/${divs.textContent.trim()}/likes`;
+        console.log("yay")
+        fetch(url, { method: 'post' })
+            .then(response => response.json())
+            .then((data) => {
+                numOfLikes.textContent = data.likes
+            })
+    })
+
+}
